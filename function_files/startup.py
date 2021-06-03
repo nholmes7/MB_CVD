92f6dd9a12d7715520bc97c5e347308172e4c0e9
nholmes7 <nholmes7@uwo.ca>
2021-06-02 21:44:38 -0400

    Code reorganization Updates to startup

--- function_files/startup.py
+++ function_files/startup.py
@@ -1,20 +1,44 @@
-import serial
-from MFC_functions import *
+'''
+Set up instances for the various pieces of equipment in the network.  Ping all
+networked devices to ensure we have communication before we begin running our
+program.
 
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate=9600,timeout=3)
+Functions
+---------
 
-# Ping all the devices on the network to make sure we have 
-# communication with each before we begin running our program.
+    startup()
 
-equipment = {
-    'MFC_C2H4':'101',
-    'MFC_Ar':'102',
-    'MFC_He':'103'
-    # 'MFC_H2':'104'
-}
+'''
 
-def startup(equipment):
-    for device in equipment:
-        operating_mode = MFC_command(equipment[device],'query_operating_mode')
+# devices = {
+#     'MFC_C2H4':'101',
+#     'MFC_Ar':'102',
+#     'MFC_He':'103',
+#     'MFC_H2':'104',
+#     'Furnace':'150'
+# }
 
-startup(equipment)
+def startup():
+    MFC_C2H4 = MFC(101)
+    MFC_Ar = MFC(102)
+    MFC_He = MFC(103)
+    MFC_H2 = MFC(104)
+    furnace = furnace(150)
+
+    a = MFC_C2H4.QueryOpMode()
+    b = MFC_Ar.QueryOpMode()
+    c = MFC_He.QueryOpMode()
+    d = MFC_H2.QueryOpMode()
+    e = furnace.ReportStatus()
+
+    if a and b and c and d and e:
+        print('All devices online.')
+
+    # for device in devices:
+    #     operating_mode = MFC_command(devices[device],'query_operating_mode')
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append('.')
+    from function_files.equipment import *
+    startup(devices)
